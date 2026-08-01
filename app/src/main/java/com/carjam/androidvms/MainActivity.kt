7cd67188c4a7341ab58d3da15dfabc50321b7c77
carjam1200-jpg <carjam1200@gmail.com>
2026-08-01 10:35:33 -0400

    Add Android Studio Kotlin activity

---
+++ app/src/main/java/com/carjam/androidvms/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.carjam.androidvms
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        title = "Android VMs App"
+    }
+}
